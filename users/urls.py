24c95761d6485dbf365118b3533ebad096a38636
Macias92 <maciejstys92@gmail.com>
2023-01-08 21:31:53 +0100

    corrected login and logout features

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 """users URL Configuration"""
-from django.urls import path, include
-from . import views
+from django.urls import path
+from users.views import LoginView, LogoutView, register
 
 app_name = 'users'
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
-    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', register, name='register'),
     
     
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
+from django.views import View
+from users.forms import LoginForm
 
-# Create your views here.
+
 def register(request):
     if request.method == 'GET':
         form = UserCreationForm()
@@ -12,7 +14,31 @@ def register(request):
             new_user = form.save()
             login(request, new_user)
             return redirect('learning_logs:index')
-        
+
     context = {'form': form}
     return render(request, 'users/register.html', context)
-        
+
+
+class LoginView(View):
+    def get(self, request):
+        form = LoginForm()
+        return render(request, 'users/login.html', {'form': form})
+
+    def post(self, request):
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                redirect_url = request.GET.get('next', '/')
+                return redirect(redirect_url)
+            else:
+                return render(request, 'login.html', {'form': form})
+
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('/')
