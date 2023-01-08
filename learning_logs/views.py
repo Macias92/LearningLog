23caec72ffba616dd9e3f0696fa5d859fb732e4d
Macias92 <maciejstys92@gmail.com>
2023-01-08 19:41:33 +0100

    Added 'new topic', 'edit entry'

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
-from learning_logs.models import Topic
+from django.shortcuts import render, redirect
+from learning_logs.models import Topic, Entry
+from learning_logs.forms import TopicForm, EntryForm
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'index.html')
 
@@ -10,10 +13,56 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'topics.html', context)
-    
+
+
 def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'topic.html', context)
-    
+
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    context = {'form': form}
+    return render(request, 'new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method == 'GET':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+
+            return redirect('learning_logs:topic', topic_id=topic_id)
+        
+    context = {'form': form, 'topic': topic}
+    return render(request, 'new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    if request.method == 'GET':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+        
+    context = {'entry': entry, 'form': form, 'topic': topic}
+    return render(request, 'edit_entry.html', context)
+        
+
